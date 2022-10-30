e806389e30dea30e7310a2ee1abfeba40fc3189c
Diana C Peterson <dpeter10@gmail.com>
2022-10-30 19:27:32 -0400

    commit tree 7d4cd60b265be714f5dc5d77c54467ef33743161

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+let num3 = 19
+let num4 = 5
+
+const mod = num3 % num4
+
+console.log(mod)
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const max = Math.max(1, 2, 20)
